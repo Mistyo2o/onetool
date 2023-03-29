9db09393be40a71fd12fa054c4e9dd446c5be4f8
Misty <zhangfugui411@gmail.com>
2023-03-29 11:09:27 +0800

    配置文件调整

---
+++ onetool-spider/src/main/java/com/onetool/spider/dao/SongRepository.java
@@ -0,0 +1,6 @@
+package com.onetool.spider.dao;/**
+ * @author: zh
+ * @date: 2023/3/29 9:24
+ * @description:
+ */public class SongRepository {
+}
---
+++ onetool-spider/src/main/java/com/onetool/spider/service/MinIoFileService.java
@@ -0,0 +1,6 @@
+package com.onetool.spider.service;/**
+ * @author: zh
+ * @date: 2023/3/29 11:00
+ * @description:
+ */public class MinIoFileService {
+}
